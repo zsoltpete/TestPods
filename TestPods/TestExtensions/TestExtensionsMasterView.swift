2ca91d43a6f63613b6c641f246c90fbe5c0d8c52
petez <pete.zsolt@codeyard.eu>
2017-11-18 22:40:45 +0100

    🔥 Removed init ViewController

---
+++ TestPods/TestExtensions/TestExtensionsMasterView.swift
@@ -0,0 +1,21 @@
+//
+//  TestExtensionsMasterView.swift
+//  TestPods
+//
+//  Created by Zsolt Pete on 2017. 11. 18..
+//  Copyright © 2017. Zsolt Pete. All rights reserved.
+//
+
+import UIKit
+
+class TestExtensionsMasterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
